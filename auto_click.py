2684ca11512522ba7603e19f874e3f47c0fe6caf
efrdtegf <vtay2@atomicmail.io>
2026-02-06 19:03:15 +0800

    Update auto_click.py

--- auto_click.py
+++ auto_click.py
@@ -56,7 +56,7 @@ clean_old_logs()
 
 # 主逻辑开始
 try:
-    driver.get("https://onlyno999.streamlit.app/")
+    driver.get("streamlit0202.cpro5.dpdns.org")
     print("已打开网页，等待页面加载 30 秒...")
     time.sleep(30)  # 初次加载等待
 
